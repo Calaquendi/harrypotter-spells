d718e88994a8b8a0ebc6e0372f20837bf9f990ce
Calaquendi <ieva@hogvartsas.lt>
2020-04-21 20:41:53 +0300

    Add migrations for types and spells tables

---
+++ database/migrations/2020_04_21_172701_create_spells_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpellsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spells', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('full_name');
+            $table->integer('type');
+            $table->string('color');
+            $table->string('effect');
+            $table->text('description');
+            $table->text('quote');
+            $table->text('history');
+            $table->text('effect_full');
+            $table->string('additional_title');
+            $table->text('additional');
+            $table->text('etymology');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spells');
+    }
+}
