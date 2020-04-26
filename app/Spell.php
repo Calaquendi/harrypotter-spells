41a5fbd5d33bc945d30ffce584c6aa3704345360
Calaquendi <ieva@hogvartsas.lt>
2020-04-26 18:12:03 +0300

    Add relationship for spells

--- app/Spell.php
+++ app/Spell.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Spell extends Model
 {
     protected $fillable = ['name', 'full_name', 'type', 'color', 'effect', 'description', 'quote', 'history', 'effect_full', 'additional_title', 'additional', 'etymology'];
+    
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
