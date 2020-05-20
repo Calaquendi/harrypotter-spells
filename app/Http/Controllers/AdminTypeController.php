2e8d714c63ae7aea0a4873dbb6a541adbb484103
Calaquendi <ieva@hogvartsas.lt>
2020-05-20 13:05:36 +0300

    Rename TypeController to AdminTypeController

---
+++ app/Http/Controllers/AdminTypeController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTypeRequest;
+use App\Type;
+use Illuminate\Http\Request;
+
+class AdminTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $types = Type::all();
+
+        return view('admin.types.index', compact('types'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.types.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTypeRequest $request)
+    {
+
+        Type::create([
+            'name' => $request->name,
+            'type_url' => $request->type_url
+        ]);
+
+        return redirect()->route('types.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $type = Type::findOrFail($id);
+
+        return view('admin.types.edit', compact('type'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreTypeRequest $request, $id)
+    {
+        $type = Type::findOrFail($id);
+        $type->update([
+            'name' => $request->name,
+            'type_url' => $request->type_url
+        ]);
+
+        return redirect()->route('types.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $type = Type::findOrFail($id);
+        $type->delete();
+
+        return redirect()->route('types.index');
+    }
+}
