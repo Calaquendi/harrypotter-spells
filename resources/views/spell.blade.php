135f5145c420062f62f18a8a0c2fa2e23ff3d333
Calaquendi <ieva@hogvartsas.lt>
2020-08-01 16:02:17 +0300

    Add If-Empty Checks for Spell Sections

--- resources/views/spell.blade.php
+++ resources/views/spell.blade.php
@@ -4,26 +4,42 @@
 <!-- Page Features -->
 
 @foreach ($spells as $spell)
-<h1 class="my-4">{{ $spell->name }}</h1>
+<h1 class="my-4 text-center" id="spellTitle">{{ $spell->name }}</h1>
 
 <div class="container">
     <div class="row">
         <div class="col-sm-3">
-            <div id="spellName">{{ $spell->full_name }}</div>
-            <div id="typeName">{{ $spell->type->name }}</div>
-            <div id="spellColor">{{ $spell->color }}</div>
-            <div id="spellEffect">{{ $spell->effect }}</div>
+            <div id="spellName" class="badge badge-primary mb-2">{{ $spell->full_name }}</div>
+            <div class="list-group">
+            <div id="typeName" class="list-group-item list-group-item-action list-group-item-dark"><strong>Tipas:</strong> {{ $spell->type->name }}</div>
+            <div id="spellColor" class="list-group-item list-group-item-action list-group-item-dark"><strong>Šviesa:</strong> {{ $spell->color }}</div>
+            <div id="spellEffect" class="list-group-item list-group-item-action list-group-item-dark"><strong>Efektas:</strong> {{ $spell->effect }}</div>
+</div>
         </div>
         <div class="col-sm-9">
+            <h3 class="spellBlockTitle">Aprašymas</h3>
             <div id="spellDesc">{{ $spell->description }}</div>
-            <div>{{ $spell->quote }}</div>
+            <blockquote class="blockquote">
+                <p class="mb-0 mt-4 font-italic font-weight-light">{{ $spell->quote }}</p>
+            </blockquote>
         </div>
         <div class="col-sm-12">
-            <div id="spellHistory">{{ $spell->history }}</div>
-            <div id="spellEffectFull">{{ $spell->effect_full }}</div>
-            <div id="spellEffect">{{ $spell->additional_title }}</div>
-            <div id="spellEffect">{{ $spell->additional }}</div>
-            <div id="spellEffect">{{ $spell->etymology }}</div>
+            @if( !empty($spell->history))
+            <h3 class="spellBlockTitle">Istorija</h3>
+            <div class="spellInfo">{{ $spell->history }}</div>
+            @endif
+            @if( !empty($spell->effect_full))
+            <h3 class="spellBlockTitle">Kerėjimas ir poveikis</h3>
+            <div class="spellInfo">{{ $spell->effect_full }}</div>
+            @endif
+            @if( !empty($spell->additional))
+            <h3 class="spellBlockTitle">{{ $spell->additional_title }}</h3>
+            <div class="spellInfo">{!! $spell->additional !!}</div>
+            @endif
+            @if( !empty($spell->etymology))
+            <h3 class="spellBlockTitle">Žodžio kilmė</h3>
+            <div class="spellInfo">{{ $spell->etymology }}</div>
+            @endif
         </div>
     </div>
 </div <!-- /.row -->
