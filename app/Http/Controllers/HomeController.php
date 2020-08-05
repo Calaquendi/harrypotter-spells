c52551a97d5be66ab015a8b460dcfbb29125b147
Vladimiras <vladimiras.petrovas@gmail.com>
2020-08-05 17:09:52 +0300

    Revert "fix: frontpage file locations"
    This reverts commit 11d9bcf3df703de1fc05cb81aaa42eff45f9835a.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,15 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Type;
-use App\Spell;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $spells = Spell::all();
-        $types = Type::all();
-        return view('index', compact('types', 'spells') );
+        return view('index');
     }
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,9 +13,6 @@
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
-    <!-- Fontawesome Kit JS -->
-    <script src="https://kit.fontawesome.com/e354dbdd31.js" crossorigin="anonymous"></script>
-
     <!-- Custom styles for this template -->
     <link href="{{ asset('css/spellbook.css') }}" rel="stylesheet">
 
@@ -26,7 +23,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#">Burtažodžiai.lt</a>
+            <a class="navbar-brand" href="#">Start Bootstrap</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
