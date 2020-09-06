ff35712398f8649c0f146bedc04720341fa5e76f
Vladimiras <vladimiras.petrovas@gmail.com>
2020-09-06 17:55:10 +0300

    fix empty search error

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -17,24 +17,12 @@ class SearchController extends Controller
 	 */
     public function postSearch(Request $request)
     {
-        /*
-        $meta_title = "Search results";
         $search = $request->input('search');
-        $query = Spell::where('id', 'like', '%'.$search.'%')
-                        ->orWhere('name', 'like', '%'.$search.'%')
-                        ->orWhere('spell_url', 'like', '%'.$search.'%')
-                        ->orWhere('full_name', 'like', '%'.$search.'%')
-                        ->orWhere('effect', 'like', '%'.$search.'%')
-                        ->orWhere('effect_full', 'like', '%'.$search.'%')
-                        ->orWhere('history', 'like', '%'.$search.'%')
-                        ->orWhere('additional_title', 'like', '%'.$search.'%');
-        $spells = $query->get()->sortByDesc("created_at");
-
-        return view('search', compact('spells', 'search'));
-        */
-        $search = $request->input('search');
-
-        return redirect('search/'.$search);
+        if ($search == null) {
+            return back()->withInput();
+        } else {
+            return redirect('search/'.$search);
+        }
     }
 
     /**
