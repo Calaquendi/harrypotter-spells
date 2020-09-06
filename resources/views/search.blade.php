92e5ff30be13340589a8ccfd52b1f4ab8226d08e
Calaquendi <ieva@hogvartsas.lt>
2020-09-06 17:24:46 +0300

    Remove unnecessary comments

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <div class="container">
-<!-- Page Features -->
 
 <div class="row justify-content-center mb-3">
     <div class="alert alert-info" role="alert">
@@ -43,41 +42,6 @@
                         <a href="{{ asset ($spell->type->type_url .'/'. $spell->spell_url) }}" class="stretched-link"></a>
                     </div>
                 </div>
-            
-            <!--
-                <h1 class="display-4 my-4 text-center" id="spellTitle">{{ $spell->name }}</h1>
-            <div class="col-sm-3">
-                <div id="spellName" class="badge badge-primary mb-2">{{ $spell->full_name }}</div>
-                <div id="typeName"><span class="lead text-primary2 text-uppercase">Tipas:</span> <strong>{{ $spell->type->name }}</strong></div>
-                <div id="spellColor"><span class="lead text-primary2 text-uppercase">Šviesa:</span> <strong>{{ $spell->color }}</strong></div>
-                <div id="spellEffect"><span class="lead text-primary2 text-uppercase">Efektas:</span> <strong>{{ $spell->effect }}</strong></div>
-            </div>
-            <div class="col-sm-9">
-                <h3 class="spellBlockTitle">Aprašymas</h3>
-                <div id="spellDesc">{{ $spell->description }}</div>
-                <blockquote class="blockquote">
-                    <p class="mb-0 mt-4 font-italic font-weight-light">{{ $spell->quote }}</p>
-                </blockquote>
-            </div>
-            <div class="col-sm-12">
-                @if( !empty($spell->history))
-                <h3 class="spellBlockTitle">Istorija</h3>
-                <div class="spellInfo">{{ $spell->history }}</div>
-                @endif
-                @if( !empty($spell->effect_full))
-                <h3 class="spellBlockTitle">Kerėjimas ir poveikis</h3>
-                <div class="spellInfo">{{ $spell->effect_full }}</div>
-                @endif
-                @if( !empty($spell->additional))
-                <h3 class="spellBlockTitle">{{ $spell->additional_title }}</h3>
-                <div class="spellInfo">{!! $spell->additional !!}</div>
-                @endif
-                @if( !empty($spell->etymology))
-                <h3 class="spellBlockTitle">Žodžio kilmė</h3>
-                <div class="spellInfo">{{ $spell->etymology }}</div>
-                @endif
-            </div>
-            -->
         </div>
     </div <!-- /.row -->
     @endforeach
