7c92671e3b9f35293d072b24b43552e4d036a9f6
Calaquendi <ieva@hogvartsas.lt>
2020-09-06 18:03:35 +0300

    Change search area text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,7 @@
         {{ csrf_field() }}
         <div class="row align-items-center">
             <div class="input-group mb-4">
-                <input id="input-search" name="search" type="text" class="form-control form-control-lg" placeholder="Burtažodžio paieška" aria-label="Burtažodžio paieška" aria-describedby="button-search">
+                <input id="input-search" name="search" type="text" class="form-control form-control-lg" placeholder="Kokio burtažodžio ieškai?" aria-label="Burtažodžio paieška" aria-describedby="button-search">
                 <div class="input-group-append">
                     <button id="button-search" type="submit" class="btn btn-outline-light"><i class="fas fa-search"></i></button>
                 </div>
