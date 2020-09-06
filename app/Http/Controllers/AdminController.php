ab108c02e604a0485609cfb10dc7634895ed7083
Vladimiras <vladimiras.petrovas@gmail.com>
2020-09-06 16:32:19 +0300

    add log preview in admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Spell;
 use App\Type;
 use App\User;
+use App\Log_search;
+use App\Log_spell;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -29,7 +31,21 @@ class AdminController extends Controller
         $spells = Spell::all();
         $types = Type::all();
         $users = User::all();
+        $log_spells = Log_spell::all();
+        $log_searches = Log_search::all();
+        $log_spells_top = Log_spell::orderBy('visits', 'DESC')->limit(20)->get();
+        $log_searches_top = Log_search::orderBy('visits', 'DESC')->limit(10)->get();
+        $log_last_searches = Log_search::orderBy('updated_at', 'DESC')->limit(10)->get();
 
-        return view('admin.index', compact('spells', 'types', 'users'));
+        return view('admin.index', compact(
+                    'spells',
+                    'types',
+                    'users',
+                    'log_spells',
+                    'log_searches',
+                    'log_spells_top',
+                    'log_searches_top',
+                    'log_last_searches',
+                ));
     }
 }
