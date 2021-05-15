079c160755dbbf395d07ff6ccf1e7179bb376f24
Vladimiras <vladimiras.petrovas@gmail.com>
2021-05-15 20:16:35 +0300

    add sitemap crawler

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
+use Spatie\Sitemap\Tags\Sitemap;
 
 class GenerateSitemap extends Command
 {
@@ -28,8 +30,18 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
+        $appurl = config('app.url');
+        $sitemapFile = 'sitemap.xml';
         // modify this to your own needs
-        SitemapGenerator::create(config('app.url'))
-            ->writeToFile(public_path('sitemap.xml'));
+
+        SitemapGenerator::create($appurl)
+            ->writeToFile(public_path($sitemapFile));
+    /*
+        SitemapGenerator::create($appurl)
+            ->getSitemap()
+            // here we add one extra link, but you can add as many as you'd like
+            ->add(Url::create('/kerai')->setPriority(0.5))
+            ->writeToFile(public_path($sitemapFile));
+      */      
     }
 }
