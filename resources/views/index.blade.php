aa726479bacb6cc79a96c63ab45790406321bee1
Vladimiras <vladimiras.petrovas@gmail.com>
2021-05-22 00:00:22 +0300

    remove paralax dom, move search input

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,6 @@
 @extends('app')
 
-<div class="d-none d-md-block" style="transform: scale(1.2); position: fixed; top: -10rem;">
-    <div id="parallax-scene">
-        <div data-depth="0.04"><img src="{{ asset('img/parallax/parallax-bg-1.png') }}"></div>
-        <div data-depth="0.06"><img src="{{ asset('img/parallax/parallax-bg-2.png') }}"></div>
-        <div data-depth="0.09"><img src="{{ asset('img/parallax/parallax-bg-3.png') }}"></div>
-        <div data-depth="0.2"><img src="{{ asset('img/parallax/parallax-bg-4.png') }}"></div>
-        <div data-depth="0.4"><img src="{{ asset('img/parallax/parallax-bg-5.png') }}"></div>
-    </div>
-</div>
+<div class="bg-main"></div>
 
 @section('content')
 <div class="container">
@@ -23,13 +15,13 @@
         <h1 class="display-4 text-uppercase input-group justify-content-center" id="main-title">Burtažodžiai</h1>
     </header>
 
-    <div style="margin-top:15vh;">
+    <div style="margin-top:3vh;">
         <!-- Search -->
         {!! Form::open(array('url' => 'search', 'files' => true)) !!}
         {{ csrf_field() }}
         <div class="row align-items-center">
-            <div class="input-group mb-4">
-                <input id="input-search" name="search" type="text" class="form-control form-control-lg" placeholder="Kokio burtažodžio ieškai?" aria-label="Burtažodžio paieška" aria-describedby="button-search">
+            <div class="input-group mb-4 mx-2">
+                <input id="input-search" name="search" type="text" class="form-control form-control-lg" placeholder="Kokio burtažodžio ieškai?" aria-label="Burtažodžio paieška" aria-describedby="button-search" autocomplete="off">
                 <div class="input-group-append">
                     <button id="button-search" type="submit" class="btn btn-outline-light"><i class="fas fa-search"></i></button>
                 </div>
