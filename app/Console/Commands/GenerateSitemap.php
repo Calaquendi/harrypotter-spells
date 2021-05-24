0a64d960ecf4d99799dac0e07671e1b07edf4bf5
Vladimiras <vladimiras.petrovas@gmail.com>
2021-05-24 22:57:40 +0300

    add sitemap index (menu.xml, spells.xml)

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -3,9 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\SitemapIndex;
+use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Tags\Url;
-use Spatie\Sitemap\Tags\Sitemap;
+use App\Type;
+use App\Spell;
 
 class GenerateSitemap extends Command
 {
@@ -32,16 +34,56 @@ class GenerateSitemap extends Command
     {
         $appurl = config('app.url');
         $sitemapFile = 'sitemap.xml';
-        // modify this to your own needs
+        $indexMenu = 'menu.xml';
+        $indexSpells = 'spells.xml';
+        $indexImages = 'images.xml';
 
-        SitemapGenerator::create($appurl)
+        //generate sitemap.xml with index files
+        SitemapIndex::create()
+            ->add($appurl . '/' . $indexMenu)
+            ->add($appurl . '/' . $indexSpells)
+            //->add($appurl . '/' . $indexImages)
             ->writeToFile(public_path($sitemapFile));
-    /*
-        SitemapGenerator::create($appurl)
-            ->getSitemap()
-            // here we add one extra link, but you can add as many as you'd like
-            ->add(Url::create('/kerai')->setPriority(0.5))
-            ->writeToFile(public_path($sitemapFile));
-      */      
+
+        //generate menu.xml sitemap
+        $menu_sitemap = Sitemap::create()
+            ->add(Url::create('/'));
+        $types = Type::all();
+        foreach ($types as $type) {
+
+            if($type->parent == 0) {
+                $menu_sitemap->add(Url::create($type->type_url)->setPriority(0.8));
+            }
+            else {
+                $parent = Type::findOrFail($type->parent);
+                $url = $parent->type_url . '/' . $type->type_url;
+                $menu_sitemap->add(Url::create($url)->setPriority(0.8)); 
+            }
+        }
+        $menu_sitemap->writeToFile(public_path($indexMenu));
+
+        //generate spells.xml sitemap
+        $spells_sitemap = Sitemap::create();
+        $spells = Spell::all();
+        foreach ($spells as $spell) {
+            $type = Type::findOrFail($spell->type_id);
+            $spells_sitemap->add(Url::create($type->type_url . '/' . $spell->spell_url)->setPriority(0.9));
+        }
+        $spells_sitemap->writeToFile(public_path($indexSpells));
+
+        //generate images.xml sitemap
+        /*
+        $images_sitemap = Sitemap::create();
+        $spells = Spell::all();
+        foreach ($spells as $key => $spell) {
+            if ($key == 0) {
+                $type = Type::findOrFail($spell->type_id);
+                $images_sitemap->add(Url::create($type->type_url . '/' . $spell->spell_url)->setChangeFrequency(0)->setPriority(0));
+                $images_sitemap->add("image");
+                dump($spell->img);
+            }
+        }
+        $images_sitemap->writeToFile(public_path($indexImages));
+        */
     }
 }
